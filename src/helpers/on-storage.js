cacd2764f65b96d88cc72e8eb0b7cd5298cd5923
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-04-11 00:44:37 +0300

    Upgrade code task-03

--- src/helpers/on-storage.js
+++ src/helpers/on-storage.js
@@ -1,7 +1,7 @@
-const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
+// const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
 
-const load = key => JSON.parse(localStorage.getItem(key)) ?? {};
+// const load = key => JSON.parse(localStorage.getItem(key)) ?? {};
 
-const remove = key => localStorage.removeItem(key);
+// const remove = key => localStorage.removeItem(key);
 
-export { save, load, remove };
+// export { save, load, remove };
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,55 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+updateIput();
+feedbackForm.addEventListener('input', throttle(onSaveStorage, 500));
+
+function onSaveStorage() {
+  const feedbackFormValue = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormValue));
+}
+
+// function inStorage() {
+//   return (storageValue =
+//     JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {});
+// }
+
+feedbackForm.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const storageValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {};
+
+  if (!storageValue.email || !storageValue.message) {
+    alert('Please fill in all the fields!');
+  } else {
+    console.log(storageValue);
+
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    feedbackForm.reset();
+  }
+}
+
+function updateIput() {
+  const storageValue = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {};
+
+  emailValue = storageValue.email || '';
+  email.value = emailValue;
+
+  messageValue = storageValue.message || '';
+  message.value = messageValue;
+}
+
+//! Варіант 2 - "storageValue is not defined"
 // import throttle from 'lodash.throttle';
 // import { save, load, remove } from '../helpers/on-storage';
 
@@ -41,56 +93,3 @@
 //   messageValue = storageValue.message || '';
 //   message.value = messageValue;
 // }
-
-//! Варіант 2 - "storageValue is not defined"
-
-import throttle from 'lodash.throttle';
-
-const feedbackForm = document.querySelector('.feedback-form');
-const email = document.querySelector('.feedback-form input');
-const message = document.querySelector('.feedback-form textarea');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-
-updateIput();
-feedbackForm.addEventListener('input', throttle(onSaveStorage, 500));
-
-function onSaveStorage() {
-  const feedbackFormValue = {
-    email: email.value,
-    message: message.value,
-  };
-
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormValue));
-}
-
-function inStorage() {
-  return (storageValue =
-    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {});
-}
-
-feedbackForm.addEventListener('submit', onSubmit);
-
-function onSubmit(evt) {
-  evt.preventDefault();
-
-  inStorage();
-
-  if (!storageValue.email || !storageValue.message) {
-    alert('Please fill in all the fields!');
-  } else {
-    console.log(storageValue);
-
-    localStorage.removeItem(LOCALSTORAGE_KEY);
-    feedbackForm.reset();
-  }
-}
-
-function updateIput() {
-  inStorage();
-
-  emailValue = storageValue.email || '';
-  email.value = emailValue;
-
-  messageValue = storageValue.message || '';
-  message.value = messageValue;
-}
