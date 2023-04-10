fa8ffa9b7a2ee4fd9bc6c24c0360df4927887d1c
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-04-10 20:13:33 +0300

    Adds a solution to task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+updateOutput();
+feedbackForm.addEventListener('input', throttle(onSaveStorage, 500));
+
+function onSaveStorage() {
+  const feedbackFormValue = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(feedbackFormValue));
+}
+
+feedbackForm.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const storageSubmitedValue =
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? {};
+
+  console.log(storageSubmitedValue);
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  feedbackForm.reset();
+}
+
+function updateOutput() {
+  const storageSavedValue =
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) ?? [];
+
+  emailValue = storageSavedValue.email || '';
+  email.value = emailValue;
+
+  messageValue = storageSavedValue.message || '';
+  message.value = messageValue;
+}
