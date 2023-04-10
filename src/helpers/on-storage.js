933fb37ea6c84708d50c1b12c6d070c685ba2d51
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-04-11 00:26:09 +0300

    Optimise code

--- src/helpers/on-storage.js
+++ src/helpers/on-storage.js
@@ -1,7 +1,8 @@
 const save = (key, value) => localStorage.setItem(key, JSON.stringify(value));
 
-const load = key =>
-  (storageValue = JSON.parse(localStorage.getItem(key)) ?? {});
+const load = key => {
+  return (storageValue = JSON.parse(localStorage.getItem(key)) ?? {});
+};
 
 const remove = key => localStorage.removeItem(key);
 
